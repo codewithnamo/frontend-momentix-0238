cb9ebd5311df3c356c7ab398b0d0fec4fa50b1cb
Aditya Arora <arora.aditya520@gmail.com>
2022-08-09 14:44:59 -0400

    added hardhat

---
+++ contracts/SBTicket.sol
@@ -0,0 +1,22 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract SBTicket is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SBTicket", "SBT") {}
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal virtual override {
+        require(from == address(0), "Err: token transfer is BLOCKED");
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+}
